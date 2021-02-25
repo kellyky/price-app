606f7a82af8a931fc5e1c05021c54351b32b03c8
Kelly Young <kkristineyoung@gmail.com>
2021-02-25 08:02:57 -0600

    Adds form

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
+// Business Logic:
+const salesTax = function(taxRate) {
+  return 1 + taxRate;
+}
+
+
+
+// Interface Logic:
 $(document).ready(function() {
+  $("form#calculator").submit(function(event) {
+    event.preventDefault();
+    $("#form-results").show();
+
+
+
+  })
 
 });
